4eebde7cb8fdc5b70188eca711fd4c3f58be0c1a
Missoum Youcef <missoumleetcoding@gmail.com>
2023-09-16 08:09:11 +0100

    se commit

--- a.py
+++ a.py
@@ -1,11 +1,11 @@
-import cv2
-import pytesseract
 import pyttsx3
 import speech_recognition as sr
-from PIL import Image
+import openai
 
+# Define your OpenAI GPT-3.5 API key
+openai.api_key ='your_api_key'
 
-# user 
+# user
 
 # Initialize the text-to-speech engine
 
@@ -19,12 +19,27 @@ recognizer = sr.Recognizer()
 
 # extract places fom QR codes example
 
-Places = ['office', 'office1', 'office2']
+places = [
+    {
+        "name": "office",
+        "description": """
+                            To get to office 1 go toward along the corrider and take the elevator,
+                            walk right you will find the office.
+                            """,
+    },
+]
+
+
+
+place_guide_description=''
+
+# First section
+# In this section we ask user about his distination
+# User Side
 
 while True:
-
     # System ask user where u want to go ?
-    engine.say('Hi, where do you want to go?')
+    engine.say("Hi, where do you want to go?")
     engine.runAndWait()
 
     # Listen for the user's response
@@ -37,14 +52,20 @@ while True:
         user_input = recognizer.recognize_google(audio)
         print("You said:", user_input)
 
-        # Check if the user's response matches any of the places in Places list
-        if any(place in user_input.lower() for place in Places):
-            engine.say(f"Ok, i will guid.")
-            engine.runAndWait()
-            break  # Exit the loop if a place is recognized
-        else:
+        e = False
+        for place in places:
+            if place["name"] in user_input:
+                place_guide_description=place["description"]## Accessing description of the place:
+                engine.say(f"Ok, i am going to guide to ${place['name']}")
+                engine.runAndWait()
+                e = True
+                break
+
+        if e is False:
             engine.say("Sorry, I did not understand. Please try again.")
             engine.runAndWait()
+        else:
+            break
 
     except sr.UnknownValueError:
         engine.say("Sorry, I could not understand your speech. Please try again.")
@@ -54,37 +75,85 @@ while True:
         engine.runAndWait()
 
 
-# extract images fom QR codes example
-
-# pytesseract.pytesseract.tesseract_cmd = 'C://T/tesseract.exe'  # your path may be different
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+# Second Part
+# User's Camera will be launch and start detecting object and chatgpt try to advice user what can do.
 
 
-image=Image.open('a2.jpg')
 
-# Perform optical character recognition (OCR) on the image to extract text
-text = pytesseract.image_to_string(image)
-print(text)
+# Example of function that using YOLO of detect from video and image objects AI,
+# We assume that we used it 
+# For demonstration, let's assume you have a list of detected objects
 
-# Define a dictionary mapping recognized text to audio guidance
-text_to_audio_guidance = {
-    "exit sign": "The exit sign is to your left. Follow the corridor to exit the building.",
-    "wow": "The elevator is straight ahead. Press the button to call the elevator.",
-    # Add more mappings for objects and locations in your workplace
-}
+def detect_objects_from_video_or_image():
+    return ['elevator', 'corridor','chair']  
 
-# Convert the recognized text to lowercase for case-insensitive comparison
-recognized_text_lower = text.lower()
-# Check if the recognized text matches any object/location in the dictionary (case-insensitive)
-if recognized_text_lower in text_to_audio_guidance:
-    guidance = text_to_audio_guidance[recognized_text_lower]
-    print(f"Recognized: {text}")
+
+detected_objects = detect_objects_from_video_or_image()
+
+
+# generate_explanation
+part_of_place_guide_description=place_guide_description.split(',')[0].strip()# will be = To get to office 1 go toward along the corrider and take the elevator,
+
+def generate_explanation(msg_array, object_name):
+    prompt = f"""
+    we have a blind in a workplace and we want to guide him to a place so:
+    act as Ai guider and Based on {part_of_place_guide_description}, try to Explain to him {object_name}.
+    for example like this:
+    "chair": "There is a chair nearby you can site down.",
+    please just like this, don't generate any other sentence
+    """
+    msg_array.append(
+       {"role": "user", "content": prompt}
+    )
+    response = openai.ChatCompletion.create(
+       model="gpt-3.5-turbo",
+       max_tokens=70,
+       temperature=0.7,
+       frequency_penalty=0.5,
+       presence_penalty=0.5,
+       messages=msg_array,
+    )
+    msg_array.append(response['choices'][0]['message'])
+    response = response['choices'][0]['message']['content']
+    return response
+
+
+object_to_audio_guidance={}
+
+# Generate explanations for each object/location
+
+for object_name in detected_objects:
+    explanation = generate_explanation([], object_name)
+    object_to_audio_guidance[object_name] = explanation
+    print(f"{object_name} Explanation: {explanation}")
+
+
+# Example of object_to_audio_guidance fro chat gpt
+
+# object_to_audio_guidance = {
+#     'elevator': "There is an elevator in front of you. You can enter it to move between floors.",
+#     'Chair': "There is a chair in front of you. You can sit down if needed."
+# }
+
+
+# Iterate through the detected objects and provide audio guidance if recognized
+
+
+for obj_class in object_to_audio_guidance:
+    guidance = object_to_audio_guidance[obj_class]
+    print(f"Detected Object: {obj_class}")
     print(f"Audio Guidance: {guidance}")
     engine.say(guidance)  # Speak the guidance
-else:
-    print("Object/location not recognized.")
-    engine.say("Location not recognized.")  
+    engine.runAndWait()
+
 
 # Clean up resources
 engine.stop()
+
+
+
+
+
+
+
+
